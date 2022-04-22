a20c0797f55c671f45314258bbc63189b9a15c73
Kaylee Guise <guisekayl@gmail.com>
2022-04-22 16:03:29 -0500

    Completed POST new endpoint; formatting changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Imports //
+// IMPORTS //
 
 const express = require("express");
 const repoContext = require("./repository/repository-wrapper");
@@ -6,15 +6,17 @@ const app = express();
 
 
 
-// Middleware //
+// MIDDLEWARE //
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
-// Endpoints //
+// ENDPOINTS //
 // http://localhost:5005/api/products
 
 // GET all products
+// http://localhost:5005/api/products
 app.get("/api/products", (req,res) => {
     const products = repoContext.products.findAllProducts();
     res.send(products);
@@ -28,10 +30,18 @@ app.get("/api/products/:id", (req, res) => {
     return res.send(product);
 });
 
-// Starting a Server //
+// POST new product
+// http://localhost:5005/api/products
+app.post("/api/products", (req,res) => {
+    const newProduct = req.body;
+    const addedProduct = repoContext.products.createProduct(newProduct);
+    return res.status(201).send(addedProduct);
+});
+
+// STARTING A SERVER //
 
 const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port number: ${PORT}`)
+    console.log(`Server started on port number: ${PORT}`);
 });
